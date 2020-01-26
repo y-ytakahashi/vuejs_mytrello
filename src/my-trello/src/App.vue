8b56f72efcbd9353f6bb2e0a01ee30ab59f22ea2
y-tkbridge <novsleep@outlook.jp>
2020-01-26 23:30:13 +0900

    node app 作成開始

---
+++ src/my-trello/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <board />
+  </div>
+</template>
+
+<script>
+import Board from './components/Board';
+
+export default {
+  name: 'app',
+  components: {
+    // コンポーネントに登録する
+    Board
+
+  }
+}
+</script>
